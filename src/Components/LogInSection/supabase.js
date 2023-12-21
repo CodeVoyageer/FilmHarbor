251c3b2b949388fea68f8fb1245e065e43c081f0
Romek2003 <wroblewski2003r@wp.pl>
2023-12-21 23:49:02 +0100

    cofniete zmiany i env

--- src/Components/LogInSection/supabase.js
+++ src/Components/LogInSection/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
-const supabaseUrl = "https://tmjiqmfknjjmvayqgwxt.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtamlxbWZrbmpqbXZheXFnd3h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0NzY0MTEsImV4cCI6MjAxODA1MjQxMX0.nNIfIoTGZuSg0APdgDIRYBAxkpPNhlXXf2a-7O50Sd8";
+const supabaseUrl = import.meta.env.VITE_SUP_URL;
+const supabaseKey = import.meta.env.VITE_SUP_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
